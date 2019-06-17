04c31085da924529134f9de2b6a63d017ffa2c9c
Mohammed Osama Mohamed Sayed Ahmed <mohamedosama914@users.noreply.github.com>
2019-06-17 11:38:25 +0200

    Set README.md
    Samples

---
+++ Samples/CefSharp Web Browser Example.cmd
@@ -0,0 +1,60 @@
+
+
+"CefSharpBrowser.exe" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) CefSharp/73.0.3683.75 Mobile Safari/537.36" 
+
+cmd
+
+"CefSharpBrowser.exe" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) CefSharp/73.0.3683.75 Mobile Safari/537.36" 
+
+Windows 8.1 (73.0.3683.75) | (Not Activate Material) ---> CefSharpBrowser.exe --no-activate --off-screen-rendering-enabled 
+
+cmd
+
+"CefSharpBrowser.exe" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) CefSharp/73.0.3683.75 Safari/537.36" 
+
+Windows 10 (73.0.3683.75) | (Not Activate Material) ---> CefSharpBrowser.exe --no-activate --off-screen-rendering-enabled 
+
+cmd
+
+Windows 10 (73.0.3683.75) Mobile
+"CefSharpBrowser.exe" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Mobile Safari/537.36" 
+
+cmd
+
+"CefSharpBrowser.exe.lnk" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Mobile Safari/537.36" 
+
+cmd
+
+"CefSharpBrowser.exe.lnk" --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36" 
+
+cmd 
+
+--no-activate
+
+"CefSharpBrowser.exe.lnk" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36" 
+
+cmd 
+
+"(32-bit)Version73.0.3683.75(2019-06-12)\CefSharpBrowser.exe" --no-activate --user-agent="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36" 
+
+cmd
+
+"(32-bit)Version73.0.3683.75(2019-06-12)\CefSharpBrowser.exe" --no-activate --user-agent="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36" 
+
+cmd
+
+Windows 10 (Chrome/74.0.3729.169)
+"(32-bit)Version73.0.3683.75(2019-06-12)\CefSharpBrowser.exe" --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Mobile Safari/537.36"
+
+cmd 
+
+Windows 8.1 (Chrome/74.0.3729.169)
+"(32-bit)Version73.0.3683.75(2019-06-12)\CefSharpBrowser.exe"
+
+cmd 
+
+(32-bit)Version73.0.3683.75(2019-06-12)/CefSharpBrowser.exe --no-activate --off-screen-rendering-enabled --user-agent="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Mobile Safari/537.36"
+
+cmd
+
+--no-activate --off-screen-rendering-enabled
---
+++ Samples/new.md
@@ -0,0 +1,10 @@
+<div class="tabs" />
+
+```C#
+public static void Main() 
+{
+
+Main();
+
+}
+```
